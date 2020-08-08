7498de97c829cf03b8620f05c752630ddc47ed19
Maksym Knyzhenko <knighenko@gmail.com>
2020-08-08 14:04:48 +0300

    Initial commit

---
+++ app/src/main/java/com/knighenko/app2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.knighenko.app2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar=(Toolbar) findViewById(R.id.myToolbar);
+        toolbar.setTitle("OLX_Free");
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+}
