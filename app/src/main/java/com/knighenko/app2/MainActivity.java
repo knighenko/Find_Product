11ad5308e29e21034fdb9ed0c1e937ce29976ca0
Maksym Knyzhenko <knighenko@gmail.com>
2020-08-09 23:09:03 +0300

    Implement Navigation View

--- app/src/main/java/com/knighenko/app2/MainActivity.java
+++ app/src/main/java/com/knighenko/app2/MainActivity.java
@@ -2,6 +2,7 @@ package com.knighenko.app2;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,20 +10,31 @@ import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    private static final int LAYOUT = R.layout.activity_main;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        toolbar=(Toolbar) findViewById(R.id.myToolbar);
-        toolbar.setTitle("OLX_Free");
-        setSupportActionBar(toolbar);
+        setContentView(LAYOUT);
+        initToolBar();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater=getMenuInflater();
-        inflater.inflate(R.menu.menu,menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
         return true;
     }
+
+    private void initToolBar() {
+        toolbar = (Toolbar) findViewById(R.id.myToolbar);
+        toolbar.setTitle(R.string.toolBarTitle);
+        setSupportActionBar(toolbar);
+    }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+    }
+
 }
